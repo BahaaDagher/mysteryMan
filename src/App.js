f2c42706ea592b61a001b0ed71ac223578262771
BahaaDagher <bahaa.dagher99@gmail.com>
2023-10-04 08:33:36 +0300

    create react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css' ; 
+import 'bootstrap/dist/js/bootstrap.min.js' ; 
+function App() {
+  return (
+    <div>
+    hello here we go agin 
+    </div>
+  )
+}
+
+export default App;
